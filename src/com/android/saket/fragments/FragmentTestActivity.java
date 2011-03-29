b4a37098f270bbc58f6ba9a87595ad5aaaf2b36c
Saket Srivastav <saketsrivastav79@gmail.com>
2011-03-29 23:07:24 +0530

    adding string array

--- src/com/android/saket/fragments/FragmentTestActivity.java
+++ src/com/android/saket/fragments/FragmentTestActivity.java
@@ -21,7 +21,9 @@ public class FragmentTestActivity extends FragmentActivity implements OnItemClic
         
         ListView l = (ListView) findViewById(R.id.number_list);
         ArrayAdapter<String> magzTitles = new ArrayAdapter<String>(getApplicationContext(),
-        		android.R.layout.simple_list_item_1, R.array.magz_titles);
+        		android.R.layout.simple_list_item_1, new String[]{"Electrnics For You", 
+        															"Linux For You", 
+        															"Facts For you"});
         l.setAdapter(magzTitles);
         l.setOnItemClickListener(this);
     }
